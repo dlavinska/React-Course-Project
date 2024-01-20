3fd454bb7afeed6a7cb8527782d4317c33f9e0f3
Dariia Lavinska <d.lavinska@gmail.com>
2024-01-20 15:26:36 +0200

    add first step of project

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,11 +20,11 @@ const Login = () => {
     }
   });
   return (
-    <div className={"my-10 text-center sm:my-16 px-4"}>
-      <h1 className={"text-xl font-semibold mb-8 md:text-3xl"}>
+    <div className="my-10 text-center sm:my-16 px-4">
+      <h1 className="text-xl font-semibold mb-8 md:text-3xl">
         The best pizza.
         <br/>
-        <span className={"text-yellow-500"}>
+        <span className="text-yellow-500">
           Straight out of the oven, straight to you.
         </span>
       </h1>
